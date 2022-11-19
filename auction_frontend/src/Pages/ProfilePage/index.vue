37a58bca274919b925a113b155d8f42bbd59e671
Dario Russo <dario.russo@bbc.co.uk>
2022-11-19 02:08:22 +0000

    feat: Navbar template

---
+++ auction_frontend/src/Pages/ProfilePage/index.vue
@@ -0,0 +1,5 @@
+<template>
+<template>
+  <h1>Profile</h1>
+</template>
+</template>
--- auction_frontend/src/main.ts
+++ auction_frontend/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App).mount('#app')
