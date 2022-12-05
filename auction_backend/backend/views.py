f2c0ca5a988b742b7e0448bf292f3db801294844
virginia <tp18520@qmul.ac.uk>
2022-12-05 17:39:05 +0000

    FIX: login and register work properly with authentcated users

--- auction_backend/backend/views.py
+++ auction_backend/backend/views.py
@@ -5,14 +5,16 @@ from types import SimpleNamespace
 
 from django.http import HttpResponse, HttpRequest, JsonResponse, HttpResponseBadRequest
 from django.core.exceptions import BadRequest
+from django.shortcuts import get_object_or_404
 
+from backend.models import CustomUserManager
 from backend.models import User
 
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate
 
 
-def is_email_taken(email):
+def is_email_taken(email: str):
     try: 
         user = User.objects.filter(email = email).get()
         if user: 
@@ -22,7 +24,7 @@ def is_email_taken(email):
         return False
 
 @csrf_exempt 
-def register(request):
+def register(request: HttpRequest):
     if request.method == 'POST':
         data: SimpleNamespace = json.loads(request.body, object_hook=lambda d: SimpleNamespace(**d))
 
@@ -32,38 +34,36 @@ def register(request):
         try:
             user: Dict[str, str]= User.objects.create(
                 email         = data.email,
-                name          = data.name,
-                surname       = data.surname,
-                date_of_birth = data.date_of_birth,
-                password      = data.password,
+                # name          = data.name,
+                # surname       = data.surname,
+                # date_of_birth = data.date_of_birth,
             )
+            user.set_password(data.password)
             user.save()
             return JsonResponse(user.id, safe=False)
 
         except:
             return HttpResponseBadRequest("A new account could not be created. Check that all fields are correct")
-    
+
 
 @csrf_exempt 
-def login(request):
-    if request.method == 'POST':
-        data = json.loads(request.body, object_hook=lambda d: SimpleNamespace(**d))
+def login(request: HttpRequest):
+    if request.method == 'GET':
+        data: SimpleNamespace = json.loads(request.body, object_hook=lambda d: SimpleNamespace(**d))
 
-        email         = data.email
-        password      = data.password
+        email:str         = data.email
+        password:str      = data.password
 
-        is_authenticated = authenticate_user(email, password)   
+        is_user: User = is_user_authenticated(email, password)
 
-        if is_authenticated:     
-            user = User.objects.filter(email = email).get()
-            return JsonResponse(user.id, safe=False)
+        if is_user:
+            return JsonResponse(is_user, safe=False)
 
-        ## TODO implement better error handling
-        return JsonResponse("not authenticated", safe=False)
+        return HttpResponseBadRequest("Could not authenticate. Check that credentials are correct")
 
 
-def authenticate_user(email, password):
-    user = authenticate(email=email, password=password)
+def is_user_authenticated(email, password):
+    user: User = authenticate(email=email, password=password)
     if user is not None:
-        return True
+        return user.to_dict() # a user with those credentials exists
     return False
