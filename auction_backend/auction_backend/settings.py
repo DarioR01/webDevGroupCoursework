52c1566cad6fd3d73aaa65584dfb9f7732eb42e7
virginia <tp18520@qmul.ac.uk>
2022-12-05 12:13:49 +0000

    FIX: User now inherits from AbstractUser class
    - the email field is used in place of username for authentication

--- auction_backend/auction_backend/settings.py
+++ auction_backend/auction_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,9 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'backend.apps.BackendConfig',
     'corsheaders',
 ]
+
+AUTH_USER_MODEL = 'backend.User'
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/uploads/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- auction_backend/backend/models.py
+++ auction_backend/backend/models.py
@@ -1,16 +1,56 @@
 from django.db import models
-import datetime
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    email           = models.EmailField(max_length=254, editable=True)
-    name            = models.CharField(max_length=30, editable=True)
-    surname         = models.CharField(max_length=30, editable=True)
-    image           = models.ImageField(upload_to='./uploads', editable=True)
+import datetime
+import uuid
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    username = None
+    email           = models.EmailField(max_length=254, unique=True, editable=True)
+    name            = models.CharField(max_length=30, default = uuid.uuid4, editable=True)
+    surname         = models.CharField(max_length=30, default = uuid.uuid4, editable=True)
+    image           = models.ImageField(upload_to='profile_pic', default='default.jpg', editable=True)
     date_of_birth   = models.DateField(default=datetime.date.today, editable=True)
-    password        = models.CharField(max_length=50, editable=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return f'{self.id}, {self.name}'
+        return f'{self.id}, {self.email}'
 
     
     def to_dict(self):
