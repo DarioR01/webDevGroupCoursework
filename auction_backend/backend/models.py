6fbf015d4bd343c5cd287131157b3ce26daabe35
virginia <tp18520@qmul.ac.uk>
2022-12-05 17:38:33 +0000

    Edit model comments

--- auction_backend/backend/models.py
+++ auction_backend/backend/models.py
@@ -6,26 +6,16 @@ import datetime
 import uuid
 
 class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
     def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
         if not email:
-            raise ValueError(_('The Email must be set'))
+            raise ValueError(('The Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields) # authenticate user with email instead of username
         user.set_password(password)
         user.save()
-        return user
+        return user 
 
     def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
@@ -61,4 +51,5 @@ class User(AbstractUser):
             'surname'       : self.surname,
             # 'image'         : self.image,
             'date_of_birth' : self.date_of_birth,
+            'password'      : self.password,
         }
