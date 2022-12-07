020d340ee302d2831487810d089acd14b338d56f
virginia <tp18520@qmul.ac.uk>
2022-12-07 19:31:28 +0000

    REFACTOR: move util functions in utils file

--- auction_backend/backend/urls.py
+++ auction_backend/backend/urls.py
@@ -3,11 +3,12 @@
 
 from django.urls import path
 
-from backend.views import register, login, logout
+from backend.views import register, login, logout, name
 
 app_name = 'rainfall_record'
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('item/<int:item_id>/', name, name='name'),
 ]
