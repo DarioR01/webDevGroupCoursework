ea2a9195e3967130692bd002e5540859ce22f464
Dario Russo <dario.russo@bbc.co.uk>
2022-12-16 03:58:27 +0000

    feat: login and registration styles

--- welcome/forms.py
+++ welcome/forms.py
@@ -2,13 +2,13 @@ from django import forms
 import datetime
 
 class UserRegistration(forms.Form):
-    name = forms.CharField(label="name", required=True)
-    surname = forms.CharField(label="surname", required=True)
-    email = forms.EmailField(label="email", required=True, widget=forms.EmailInput())
-    password = forms.CharField(label="password", required=True, widget=forms.PasswordInput())
-    date = forms.DateField(label="date of birthday", widget=forms.SelectDateWidget(years=range(1950, datetime.date.today().year)))
+    name = forms.CharField(label="name", required=True, widget=forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control mb-3'}))
+    surname = forms.CharField(label="surname", required=True, widget=forms.TextInput(attrs={'placeholder': 'Surname', 'class': 'form-control mb-3'}))
+    email = forms.EmailField(label="email", required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control mb-3'}))
+    password = forms.CharField(label="password", required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control mb-3'}))
+    date = forms.DateField(label="date of birthday", required=True, widget=forms.SelectDateWidget(years=range(1950, datetime.date.today().year), attrs={'placeholder': 'Password', 'class': 'form-select m-1 mb-3'}))
 
 
 class UserLogin(forms.Form):
-    email = forms.EmailField(label="email", required=True, widget=forms.EmailInput())
-    password = forms.CharField(label="password", required=True, widget=forms.PasswordInput())
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control mb-3'}))
+    password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control mb-3'}))
